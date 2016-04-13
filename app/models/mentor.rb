a54e74d98a984b5d73a0f782da5fb2b71d76e048
Carsten Zimmermann <cz@aegisnet.de>
2016-04-13 12:59:36 +0200

    WIP: get mentor's projects and applications

--- app/models/mentor.rb
+++ app/models/mentor.rb
@@ -17,4 +17,30 @@ class Mentor
     self.class.current.include? user
   end
 
+  def projects
+    @projects ||= Project.current.accepted.where(mentor_github_handle: user.github_handle)
+  end
+
+  def applications
+    @applications ||= begin
+                        hstore_query = "application_data @> hstore(:key1, :value) OR application_data @> hstore(:key2, :value)"
+                        projects.reduce(Application.none) do |relation, project|
+                          relation += Application.where(season: Season.current).
+                            where(hstore_query, key1: 'project1_id', key2: 'project2_id', value: project.id.to_s)
+                        end
+                      end
+  end
+
+  def projects_with_applications
+    @project_with_applications ||= projects.where(id: project_ids_in_applications)
+  end
+
+  private
+
+  def project_ids_in_applications
+    @project_ids_in_applications ||= applications.each_with_object([]) do |application, list|
+      [application.application_data['project1_id'], application.application_data['project2_id']]
+    end.flatten.uniq.reject(&:blank?).map(&:to_i)
+  end
+
 end
